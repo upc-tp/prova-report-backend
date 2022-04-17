efb6d33dcffc7e7f703f3055571a220253ba2bd3
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2022-04-16 20:33:42 -0500

    Add defects and test by priority

--- src/lib/services/DashboardService.ts
+++ src/lib/services/DashboardService.ts
@@ -56,9 +56,50 @@ export class DashboardService {
                 return s;
             }));
 
+            const priorities = await conn.query(`SELECT p.id, p.name,
+            (SELECT COUNT(*) 
+            FROM test_executions te
+            INNER JOIN test_cases tc
+            ON tc.id = te.test_case_id
+            and te.order = tc.last_execution
+            INNER JOIN test_suites tst
+            ON tst.id = tc.test_suite_id
+            WHERE tc.priority_id = p.id
+            and tst.project_id = ${mysql.escape(projectId)}) as num_tests
+            FROM priorities as p;`);
+
+            const testsByPriority = await Promise.all(priorities.map(async (p) => {
+                p['statuses'] = await conn.query(`SELECT t.id, t.name,
+                (SELECT COUNT(*)
+                FROM test_executions te
+                INNER JOIN test_cases tc
+                ON tc.id = te.test_case_id
+                and te.order = tc.last_execution
+                INNER JOIN test_suites tst
+                ON tst.id = tc.test_suite_id 
+                WHERE tst.project_id = ${mysql.escape(projectId)}
+                and tc.priority_id = ${mysql.escape(p.id)}
+                and te.test_state_id = t.id
+                ) as num_tests
+                from test_states t;`);
+                return p;
+            }));
+
+            const defectsByStatus = await conn.query(`SELECT ds.id, ds.name,
+            (SELECT COUNT(*) FROM defects d
+            INNER JOIN test_cases tc
+            ON tc.id = d.test_case_id
+            INNER JOIN test_suites tst
+            ON tst.id = tc.test_suite_id
+            WHERE tst.project_id = ${mysql.escape(projectId)}
+            and d.defect_state_id = ds.id) as num_defects 
+            FROM defect_states ds;`);
+
             return {
+                testsByStatus,
+                defectsByStatus,
                 testsBySeverity,
-                testsByStatus
+                testsByPriority
             };
         } catch (error) {
             console.log(error);
