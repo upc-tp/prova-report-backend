b9afd24209d783c02a4d6094e23fabf30e9681f0
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2022-05-20 00:37:01 -0500

    fix handlebar reports

---
+++ sql/clear-database.sql
@@ -0,0 +1,14 @@
+SET FOREIGN_KEY_CHECKS = 0; 
+TRUNCATE TABLE defects;
+TRUNCATE TABLE test_execution_steps;
+TRUNCATE TABLE test_executions;
+TRUNCATE TABLE user_story_criterias;
+TRUNCATE TABLE user_stories;
+TRUNCATE TABLE test_cases;
+TRUNCATE TABLE test_suites;
+TRUNCATE TABLE test_plans;
+TRUNCATE TABLE versions;
+TRUNCATE TABLE users_projects;
+TRUNCATE TABLE projects;
+TRUNCATE TABLE users;
+SET FOREIGN_KEY_CHECKS = 1; 
